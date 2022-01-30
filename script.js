aec6304b0bffca6a4e5716f7fba7dcba341d2b51
Joss <jossappletonfox@protonmail.com>
2022-01-30 17:20:04 +0000

    Add keypress events

--- script.js
+++ script.js
@@ -24,8 +24,14 @@ const changeColor = function() {
 		document.body.classList.remove(currentValue);
 	});
 
+	const colorName = this.className;
+
 	// Add the current element class to the document body (change its color).
-	document.body.classList.add(this.className);
+	document.body.classList.add(colorName);
+
+	// Update page title to reflect color choice.
+	titleCaseColorName = colorName.slice(0, 1).toUpperCase() + colorName.slice(1);
+	document.querySelector('h1').textContent = titleCaseColorName;
 
 	updateActive();
 };
@@ -33,7 +39,50 @@ const changeColor = function() {
 // Highlight active color when checkbox is checked.
 showActive.addEventListener('click', updateActive);
 
-// Change background color on clicking a menu item.
 menuItems.forEach(function(currentValue) {
+	// Change background color on clicking a menu item.
 	currentValue.addEventListener('click', changeColor);
+
+	// Close menu on clicking a menu item.
+	currentValue.addEventListener('click', function() {
+		document.querySelector('.hamburger-menu').classList.add('no-hover');
+	});
+});
+
+// Remove no-hover class when opening the menu.
+document.querySelector('.hamburger-icon').addEventListener('mouseover', function() {
+	document.querySelector('.no-hover').classList.remove('no-hover');
+});
+
+// Change background color when corresponding key is pressed.
+document.addEventListener('keypress', function(event) {
+	switch (event.key) {
+		case '0':
+			document.querySelector('.black').click();
+			break;
+		case '1':
+			document.querySelector('.red').click();
+			break;
+		case '2':
+			document.querySelector('.green').click();
+			break;
+		case '3':
+			document.querySelector('.yellow').click();
+			break;
+		case '4':
+			document.querySelector('.blue').click();
+			break;
+		case '5':
+			document.querySelector('.magenta').click();
+			break;
+		case '6':
+			document.querySelector('.cyan').click();
+			break;
+		case '7':
+			document.querySelector('.white').click();
+			break;
+		case '-':
+			location.reload();
+			break;
+	}
 });
